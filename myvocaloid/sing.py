6f5abc3f34b807d1ef9d092d5218dc8453967d9a
Code信仰P <ez.ryow@gmail.com>
2024-12-27 23:37:12 +0900

    refactor

--- myvocaloid/sing.py
+++ myvocaloid/sing.py
@@ -4,19 +4,19 @@ from combine_audio import combine_audio
 
 VOICE_DIR = "../thirdparty/ENUNU_波音リツVer2_1202/ENUNU_波音リツVer2_1202/単独音"
 
-def resolve_voice_files(text: str) -> list:
+def find_voice_files(text: str) -> list:
     files = []
     for s in text:
         files.append(f"{VOICE_DIR}/{s}.wav")
     return files
 
 
-def generate_sing_voice(files: list, SHIFT_STEPS: list) -> list:
-    assert len(files) == len(SHIFT_STEPS)
+def generate_sing_voice(files: list, shift_steps: list) -> list:
+    assert len(files) == len(shift_steps)
     ret = None
 
     for i, f in enumerate(files):
-        y = pitch_shift(f, SHIFT_STEPS[i])
+        y = pitch_shift(f, shift_steps[i])
         if ret is not None:
             y = combine_audio(ret, y)
         ret = y
@@ -25,11 +25,11 @@ def generate_sing_voice(files: list, SHIFT_STEPS: list) -> list:
 
 def main():
     text = input("Enter text: ")
-    SHIFT_STEPS = [i * 2 for i, _ in enumerate(text)]
-    voice_files = resolve_voice_files(text)
+    shift_steps = [i * 2 for i, _ in enumerate(text)]
+    voice_files = find_voice_files(text)
     print("found voice files:", voice_files)
     print("concatenating...")
-    generated = generate_sing_voice(voice_files, SHIFT_STEPS)
+    generated = generate_sing_voice(voice_files, shift_steps)
     print("concatenated")
 
     sf.write("../gen/sing.wav", generated, 44100)
