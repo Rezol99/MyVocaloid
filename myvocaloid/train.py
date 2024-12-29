4a9aee70986bbb5fa403c14d2d514e2161f73a19
Code信仰P <ez.ryow@gmail.com>
2024-12-29 22:20:27 +0900

    add encoded data load and save feature

--- myvocaloid/train.py
+++ myvocaloid/train.py
@@ -1,4 +1,7 @@
 from file_encoder import FileEncoder
+import numpy as np
+import keras
+import sys
 
 TARGET_DIR = "./thirdparty/「波音リツ」歌声データベースVer2/DATABASE"
 OUTPUT_DIR = "./master/ust/json"
@@ -6,6 +9,31 @@ OUTPUT_DIR = "./master/ust/json"
 MIN_PITCH = 30
 MAX_PITCH = 100
 
+LYRIC_INDEX_FILE = "data/lyric_indexs.npy"
+DURATION_INDEX_FILE = "data/duration_indexs.npy"
+NOTENUM_INDEX_FILE = "data/notenum_indexs.npy"
+
 if __name__ == "__main__":
-    encoder = FileEncoder(TARGET_DIR, MIN_PITCH, MAX_PITCH, OUTPUT_DIR)
-    _names, lyric_indexs, duration_indexs, notenum_indexs  = encoder.encode()
+    need_encode = False
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "encode":
+            need_encode = True
+    
+    if need_encode:
+        encoder = FileEncoder(TARGET_DIR, MIN_PITCH, MAX_PITCH, OUTPUT_DIR)
+        _names, lyric_indexs, duration_indexs, notenum_indexs  = encoder.encode()
+
+        np.save(LYRIC_INDEX_FILE, lyric_indexs)
+        np.save(DURATION_INDEX_FILE, duration_indexs)
+        np.save(NOTENUM_INDEX_FILE, notenum_indexs)
+    
+
+    lyric_indexs = np.load(LYRIC_INDEX_FILE)
+    duration_indexs = np.load(DURATION_INDEX_FILE)
+    notenum_indexs = np.load(NOTENUM_INDEX_FILE)
+
+    print(f"lyric_indexs: {lyric_indexs.shape}")
+    print(f"duration_indexs: {duration_indexs.shape}")
+    print(f"notenum_indexs: {notenum_indexs.shape}")
+
