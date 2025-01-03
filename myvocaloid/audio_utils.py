45c04b6c803ac836db5ff3d3e0dc9d4998f4bf26
Code信仰P <ez.ryow@gmail.com>
2025-01-03 13:12:07 +0900

    add vocoder

--- myvocaloid/audio_utils.py
+++ myvocaloid/audio_utils.py
@@ -2,8 +2,10 @@ import librosa
 import soundfile as sf
 
 
-SAMPLE_RATE = 1024
+SAMPLE_RATE = 16000
 N_MELS = 128
+N_FFT = 1024
+HOP_LENGTH = 256
 
 
 def load_audio(file_path, sr=SAMPLE_RATE):
@@ -14,15 +16,23 @@ def save_audio(file_path, audio, sr=SAMPLE_RATE):
     sf.write(file_path, audio, sr)
 
 
-def audio_to_mel(audio, sr=SAMPLE_RATE, n_mels=N_MELS, n_fft=1024, hop_length=256):
+def audio_to_mel(audio, sr=SAMPLE_RATE, n_mels=N_MELS, n_fft=N_FFT, hop_length=HOP_LENGTH):
     mel_spectrogram = librosa.feature.melspectrogram(
-        y=audio, sr=sr, n_mels=n_mels, n_fft=n_fft
+        y=audio, sr=sr, n_mels=n_mels, n_fft=n_fft , hop_length=hop_length
     )
     log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
     return log_mel_spectrogram
 
-def mel_to_audio(log_mel_spec, sr=SAMPLE_RATE, n_iter=32, n_fft=1024, hop_length=256):
+def mel_to_audio(log_mel_spec, sr=SAMPLE_RATE, n_iter=32, n_fft=N_FFT, hop_length=HOP_LENGTH):
+    # メルスペクトログラムをパワースペクトログラムに変換
     power_mel = librosa.db_to_power(log_mel_spec)
-    stft = librosa.feature.inverse.mel_to_stft(power_mel, sr=sr, n_fft=n_fft)
-    audio = librosa.griffinlim(stft, n_iter=n_iter)
+
+    # メルスペクトログラムから直接音声を復元
+    audio = librosa.feature.inverse.mel_to_audio(
+        power_mel,
+        sr=sr,
+        n_fft=n_fft,
+        hop_length=hop_length,
+        n_iter=n_iter,
+    )
     return audio
